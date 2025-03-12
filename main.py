7f6f3fac15e34d2c93e74d53d07033aa03e101f0
physicstjc <85329791+physicstjc@users.noreply.github.com>
2025-03-12 09:05:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,87 +1,8 @@
-"""
-
- OMRChecker
-
- Author: Udayraj Deshmukh
- Github: https://github.com/Udayraj123
-
-"""
-
-import argparse
+import streamlit as st
 import sys
 from pathlib import Path
-
 from src.entry import entry_point
 from src.logger import logger
-
-
-def parse_args():
-    # construct the argument parse and parse the arguments
-    argparser = argparse.ArgumentParser()
-
-    argparser.add_argument(
-        "-i",
-        "--inputDir",
-        default=["inputs"],
-        # https://docs.python.org/3/library/argparse.html#nargs
-        nargs="*",
-        required=False,
-        type=str,
-        dest="input_paths",
-        help="Specify an input directory.",
-    )
-
-    argparser.add_argument(
-        "-d",
-        "--debug",
-        required=False,
-        dest="debug",
-        action="store_false",
-        help="Enables debugging mode for showing detailed errors",
-    )
-
-    argparser.add_argument(
-        "-o",
-        "--outputDir",
-        default="outputs",
-        required=False,
-        dest="output_dir",
-        help="Specify an output directory.",
-    )
-
-    argparser.add_argument(
-        "-a",
-        "--autoAlign",
-        required=False,
-        dest="autoAlign",
-        action="store_true",
-        help="(experimental) Enables automatic template alignment - \
-        use if the scans show slight misalignments.",
-    )
-
-    argparser.add_argument(
-        "-l",
-        "--setLayout",
-        required=False,
-        dest="setLayout",
-        action="store_true",
-        help="Set up OMR template layout - modify your json file and \
-        run again until the template is set.",
-    )
-
-    (
-        args,
-        unknown,
-    ) = argparser.parse_known_args()
-
-    args = vars(args)
-
-    if len(unknown) > 0:
-        logger.warning(f"\nError: Unknown arguments: {unknown}", unknown)
-        argparser.print_help()
-        exit(11)
-    return args
-
 
 def entry_point_for_args(args):
     if args["debug"] is True:
@@ -93,7 +14,56 @@ def entry_point_for_args(args):
             args,
         )
 
+def main():
+    st.title("OMRChecker UI")
+
+    # Input directory
+    input_paths = st.text_area(
+        "Input Directory(s)",
+        value="inputs",
+        help="Enter the input directory or files (comma separated)."
+    )
+    input_paths = input_paths.split(',')
+
+    # Output directory
+    output_dir = st.text_input(
+        "Output Directory",
+        value="outputs",
+        help="Specify the output directory."
+    )
+
+    # Debug option
+    debug = st.checkbox("Enable Debug Mode", value=False, help="Enable debugging for detailed errors.")
+
+    # Auto Align option
+    auto_align = st.checkbox(
+        "Enable Auto Alignment (Experimental)",
+        value=False,
+        help="Enables automatic template alignment if scans are misaligned."
+    )
+
+    # Set Layout option
+    set_layout = st.checkbox(
+        "Set Layout for OMR Template",
+        value=False,
+        help="Set up the OMR template layout and modify the JSON file."
+    )
+
+    # Run button to execute the logic
+    if st.button("Run OMR Checker"):
+        # Prepare arguments for the script
+        args = {
+            "input_paths": input_paths,
+            "debug": debug,
+            "output_dir": output_dir,
+            "autoAlign": auto_align,
+            "setLayout": set_layout
+        }
+        
+        # Call the entry point function
+        entry_point_for_args(args)
+
+        st.success("OMR processing completed successfully.")
 
 if __name__ == "__main__":
-    args = parse_args()
-    entry_point_for_args(args)
+    main()
